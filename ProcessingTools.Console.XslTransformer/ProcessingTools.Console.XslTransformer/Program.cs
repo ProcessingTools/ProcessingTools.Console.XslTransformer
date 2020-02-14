fc7f47a72cd7125f535d1d3a0e3498f57ead3f49
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2020-02-14 16:17:51 +0200

    Add solution

---
+++ ProcessingTools.Console.XslTransformer/ProcessingTools.Console.XslTransformer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProcessingTools.Console.XslTransformer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
