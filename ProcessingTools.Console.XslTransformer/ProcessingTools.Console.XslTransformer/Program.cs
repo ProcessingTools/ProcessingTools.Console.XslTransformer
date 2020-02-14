b7e53f6872198e376f0e5c18fb43700a5eb36fb8
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2020-02-14 16:24:12 +0200

    Add code

--- ProcessingTools.Console.XslTransformer/ProcessingTools.Console.XslTransformer/Program.cs
+++ ProcessingTools.Console.XslTransformer/ProcessingTools.Console.XslTransformer/Program.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿// <copyright file="Program.cs" company="ProcessingTools">
+// Copyright (c) 2020 ProcessingTools. All rights reserved.
+// </copyright>
 
 namespace ProcessingTools.Console.XslTransformer
 {
-    class Program
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Xml.Xsl;
+
+    /// <summary>
+    /// Main program class.
+    /// </summary>
+    public static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Main method.
+        /// </summary>
+        /// <param name="args">Arguments.</param>
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args is null || args.Length < 2)
+            {
+                PrintHelp();
+                Environment.Exit(1);
+                return;
+            }
+
+            string xsltFileName = args[0];
+            string inputFileName = args[1];
+            string outFileName = args.Last();
+
+            if (outFileName == inputFileName)
+            {
+                outFileName = $"{Path.GetFileNameWithoutExtension(inputFileName)}.{DateTime.Now:yyyyMMddHHmmssffffff}.{Path.GetExtension(inputFileName).Trim('.')}";
+            }
+
+            XslCompiledTransform xsl = new XslCompiledTransform();
+            xsl.Load(xsltFileName);
+            xsl.Transform(inputFileName, outFileName);
+        }
+
+        private static void PrintHelp()
+        {
+            Console.WriteLine($"Usage: {AppDomain.CurrentDomain.FriendlyName} <XSLT file name> <input file name>[ <output file name>]");
         }
     }
 }
